789fb3e41e798b5b56656d169b623f5a1e7e03d0
solovdaria <44465483+solovdaria@users.noreply.github.com>
2020-11-29 22:58:30 +0200

    Add files via upload

---
+++ Multiprocessing/src/Main.java
@@ -0,0 +1,104 @@
+//package main;
+
+import java.util.Scanner;
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+
+import java.io.*;
+import java.net.*;
+import java.util.logging.*;
+
+public class Main {
+    public static Process processF;
+    public static Process processG;
+
+    public static void terminate() {
+        processF.destroy();
+        processG.destroy();
+    }
+
+    public static void resultsInZero(String funcName) {
+        System.out.println(funcName + " finished with zero value. Process is terminated.");
+        terminate();
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        //int test = Integer.parseInt("6");
+        Scanner console = new Scanner(System.in);
+        int test = console.nextInt();
+
+        System.out.println("Press '0' at any time to cancel computation.");
+        KeyListener listener = null;
+
+        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        logger.setLevel(Level.OFF);
+        logger.setUseParentHandlers(false);
+        try {
+            GlobalScreen.registerNativeHook();
+            listener = new KeyListener();
+            GlobalScreen.addNativeKeyListener(listener);
+        } catch (NativeHookException ex) {
+            System.err.println(ex.getMessage());
+        }
+
+        try {
+            ServerSocket manager = new ServerSocket(2323);
+
+            ProcessBuilder pbF = new ProcessBuilder("java", "FuncF");
+            pbF.directory(new File("C:\\Users\\user\\IdeaProjects\\Multiprocessing\\out\\production\\Multiprocessing"));
+            processF = pbF.start();
+            ProcessBuilder pbG = new ProcessBuilder("java", "FuncG");
+            pbG.directory(new File("C:\\Users\\user\\IdeaProjects\\Multiprocessing\\out\\production\\Multiprocessing"));
+            processG = pbG.start();
+
+            Socket socketF = manager.accept();
+            Socket socketG = manager.accept();
+            DataOutputStream outputF = new DataOutputStream(socketF.getOutputStream());
+            outputF.writeUTF(Integer.toString(test));
+            DataOutputStream outputG = new DataOutputStream(socketG.getOutputStream());
+            outputG.writeUTF(Integer.toString(test));
+
+            DataInputStream inputF = new DataInputStream(socketF.getInputStream());
+            DataInputStream inputG = new DataInputStream(socketG.getInputStream());;
+
+            int resultOfF = -1;
+            int resultOfG = -1;
+
+            while(true) {
+                if (inputF.available() > 0 && resultOfF == -1) {
+                    resultOfF = inputF.readByte();
+                    listener.f_is = resultOfF;
+                    if (resultOfF == 0) {
+                        resultsInZero("F");
+                        break;
+                    }
+                }
+
+                if (inputG.available() > 0 && resultOfG == -1) {
+                    resultOfG = inputG.readByte();
+                    listener.g_is = resultOfG;
+                    if (resultOfG == 0) {
+                        resultsInZero("G");
+                        break;
+                    }
+                }
+
+                if (resultOfF > 0 && resultOfG >0) break;
+            }
+            System.out.print("\nResult: " + (resultOfF | resultOfG) + "\n");
+
+            inputF.close();
+            inputG.close();
+            terminate();
+            socketF.close();
+            socketG.close();
+            manager.close();
+            GlobalScreen.unregisterNativeHook();
+        } catch (IOException e) {
+            System.out.println(e);
+        } catch (NativeHookException e) {
+            e.printStackTrace();
+        }
+    }
+}
